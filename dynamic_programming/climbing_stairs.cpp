e2dcd95ce3495cf946c778bf5c15dee229ef3ded
Galvin <68190778+galviy@users.noreply.github.com>
2026-08-12 11:10:41 +0700

    Update climbing_stairs.cpp

--- dynamic_programming/climbing_stairs.cpp
+++ dynamic_programming/climbing_stairs.cpp
@@ -3,6 +3,20 @@
 
 using namespace std;
 
+
+/*
+  int climbStairs(int n) {
+        if (n <= 2) return n;
+        vector<int> dp(n+1);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+    Tabulation (buttom up)
+*/
 class Solution {
 public:
     int fibonacci(int n, vector<int>& memo) {
